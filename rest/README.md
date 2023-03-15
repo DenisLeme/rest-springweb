de5aa672b4e893c601104a18fcc569cf515a1950
Denis Leme <42851804+DenisLeme@users.noreply.github.com>
2023-03-15 10:29:47 -0300

    rest completo feito na semana

---
+++ rest/README.md
@@ -0,0 +1,2 @@
+"# rest-springweb" 
+"# rest-springweb" 
---
+++ rest/REST-Spring/src/main/java/br/com/dio/controller/ProdutoController.java
@@ -0,0 +1,48 @@
+package br.com.dio.controller;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import br.com.dio.entity.Produto;
+import br.com.dio.service.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService service;
+
+
+	@PostMapping(value = "/save")
+	public ResponseEntity<Produto> salvaProduto(@RequestBody Produto produto) throws Exception {
+
+		produto = service.save(produto);
+
+		return ResponseEntity.ok().body(produto);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Produto> buscaProduto(@PathVariable Long id) {
+
+		Produto produto = service.findById(id);
+
+		return ResponseEntity.ok().body(produto);
+
+	}
+
+	@GetMapping(value = "/busca-todos")
+	public ResponseEntity<List<Produto>> buscaTodosProdutos() {
+
+		List<Produto> produtos = service.findAll();
+
+		return ResponseEntity.ok().body(produtos);
+
+	}
+
+}
